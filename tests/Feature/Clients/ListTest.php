11de680fa5c5beb36e9dad8149614164eda3f74f
jmrecodes <jmrecodes2022@gmail.com>
2024-11-11 20:11:57 +0800

    Improve client list page tests Expand test suite to ensure clients list page is accessible only to authenticated users. The previous test was inadequate as it only checked the page existence without proper authentication. This change ensures the clients list page is protected and users must be logged in to access it.

--- tests/Feature/Clients/ListTest.php
+++ tests/Feature/Clients/ListTest.php
@@ -1,7 +1,20 @@
 <?php
-// Example only
-it('has clients/listtest.php page', function () {
-    $response = $this->get('/clients/listtest.php');
 
+use App\Models\User;
+
+it('has a client list page', function() {
+    // create a user 
+    $user = User::factory()->create();
+
+    // go to that page with the user authenticated
+    $response = $this->actingAs($user)->get('/admin/clients');
+
+    // assert that the page was successful
     $response->assertStatus(200);
+});
+
+it('has to be authenticated', function() {
+    $response = $this->get('/admin/clients');
+
+    $response->assertStatus(302)->assertRedirect('/login');
 });
